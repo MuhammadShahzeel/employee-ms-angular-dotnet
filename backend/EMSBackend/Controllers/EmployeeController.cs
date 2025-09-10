1204468329d334d071289cdb3e3ef07e6e20bc5e
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-09-10 05:18:12 +0500

    project setup

---
+++ backend/EMSBackend/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using EMSBackend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMSBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public EmployeeController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok(_dbContext.Employees.ToList());
+        }   
+    }
+}
---
+++ backend/EMSBackend/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using EMSBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMSBackend.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet< Employee> Employees { get; set; }
+
+    }
+}
